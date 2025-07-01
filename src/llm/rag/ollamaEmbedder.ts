c84014273db8560351fce93f87bd21fcccd20512
Arren De Manuel <arrendeman@gmail.com>
2025-07-01 02:44:05 -0700

    Create VectorDb in memory

---
+++ src/llm/rag/ollamaEmbedder.ts
@@ -0,0 +1,27 @@
+import {Ollama} from "ollama";
+
+const MODEL = 'mxbai-embed-large:latest';
+
+export class OllamaEmbedder {
+
+    private ollamaInstance: Ollama;
+
+    constructor(ollamaInstance: Ollama) {
+        // initiate the chat bot
+        this.ollamaInstance = ollamaInstance
+    }
+
+    embedChunk(chunk: string) {
+        return this.ollamaInstance.embeddings({
+            model: MODEL,
+            prompt: chunk,
+        })
+    }
+
+    embedChunks(chunks: string[]) {
+        return this.ollamaInstance.embed({
+            model: MODEL,
+            input: chunks,
+        })
+    }
+}
---
+++ src/llm/rag/vectorDb.ts
@@ -0,0 +1,48 @@
+export interface VectorData {
+    chunk: string,
+    embedding: number[],
+}
+
+interface SimilarityData {
+    vector: VectorData,
+    similarity: number,
+}
+
+export class VectorDB {
+    inMemoryVectors: VectorData[] = [];
+
+    addVector(vector: VectorData) {
+        this.inMemoryVectors.push(vector);
+    }
+
+    resetVectorDb() {
+        this.inMemoryVectors = [];
+    }
+
+    retrieveSimilarVectors(inputVector: VectorData, topN: number): VectorData[] {
+        const similaritiesList: SimilarityData[] = this.inMemoryVectors.map((knowledgeVector) => {
+            const similarity = this.cosineSimilarity(inputVector, knowledgeVector);
+            return {
+                vector: knowledgeVector,
+                similarity: similarity
+            }
+        });
+
+        // sort in descending order
+        const sortedSimilarityList = similaritiesList.sort((a, b) => b.similarity - a.similarity);
+        return sortedSimilarityList.slice(0, topN).map((similarityData) => similarityData.vector)
+    }
+
+    private cosineSimilarity(vector1: VectorData, vector2: VectorData): number {
+        const embedding1 = vector1.embedding;
+        const embedding2 = vector2.embedding;
+
+        const dotProduct = embedding1.map((_, i) => {
+            return embedding1[i] * embedding2[i];
+        }).reduce((m, n) => m + n);
+        const normEmbedding1 = Math.sqrt(embedding1.map((x) => x * x).reduce((m, n) => m + n));
+        const normEmbedding2 = Math.sqrt(embedding2.map((x) => x * x).reduce((m, n) => m + n));
+
+        return dotProduct / (normEmbedding1 * normEmbedding2)
+    }
+}
